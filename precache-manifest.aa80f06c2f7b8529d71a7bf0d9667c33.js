44e86686a6ba49515cb300c3ac427667a4676fb6
karanrumalwala <32751689+karanrumalwala@users.noreply.github.com>
2019-11-13 16:18:01 -0500

    Updates

---
+++ precache-manifest.aa80f06c2f7b8529d71a7bf0d9667c33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e044dcd5585bc0d52ee71af80c1fcaf",
+    "url": "/KaranRecipes/index.html"
+  },
+  {
+    "revision": "950ec4a3d46e712b0d27",
+    "url": "/KaranRecipes/static/css/main.5a771f6c.chunk.css"
+  },
+  {
+    "revision": "22d31bf1ceac7722f880",
+    "url": "/KaranRecipes/static/js/2.6462b962.chunk.js"
+  },
+  {
+    "revision": "950ec4a3d46e712b0d27",
+    "url": "/KaranRecipes/static/js/main.e3fcf4b9.chunk.js"
+  },
+  {
+    "revision": "85eea2ac67c13c095203",
+    "url": "/KaranRecipes/static/js/runtime~main.8dd72ee8.js"
+  }
+]);
